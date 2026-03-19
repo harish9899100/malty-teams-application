5058344b838cec587b1f92e940cb282ee1ac8be7
harish khileri <harishkhileri98@gmail.com>
2026-03-19 21:30:16 +0530

    set up association for user

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,8 @@
 class AdminUser < ApplicationRecord
+  has_many :organizations, dependent: :destroy
+  has_many :teams, through: :organizations
+  has_many :users, through: :organization
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
